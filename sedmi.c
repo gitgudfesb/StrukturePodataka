4018dc1f48585d26c86691960994f508bf33d4bf
gitgudfesb <56274871+gitgudfesb@users.noreply.github.com>
2019-11-08 17:24:30 +0100

    Pocetak rada na sedmon

---
+++ sedmi.c
@@ -0,0 +1,104 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#define IZRAZ_MAX 256
+
+
+typedef struct _list
+{
+	char *element;
+	struct _list* next;
+}list;
+
+typedef list* poz;
+
+int dodajNaStog(poz p, char *c) {
+
+	poz q = (poz)malloc(sizeof(list));
+
+	q->element = c;
+
+	q->next = p->next;
+
+	p->next = q;
+
+	return 0;
+}
+
+
+int pop(poz p) {
+
+	poz trash = p->next;
+
+	if (trash == NULL) {
+		printf("Lista je vec prazna !!!");
+		return -1;
+	}
+
+	p->next = trash->next;
+
+	free(trash);
+
+
+	return 0;
+}
+
+int ispis(const poz p)
+{
+	poz elementListe = p;
+	elementListe = elementListe->next;//da ne ispisuje head(dummy) element
+	while (elementListe != NULL)
+	{
+		printf("%c, ", elementListe->element);
+		elementListe = elementListe->next;
+	}
+
+	return 0;
+}
+
+
+
+int ucitajIzDatoteke(const char *datoteka,const poz p){
+
+	char izraz[IZRAZ_MAX], c;
+	int i = 0;
+	FILE *f = fopen(datoteka, "r");
+
+	
+	if(f ==NULL)
+	{
+		printf("Dogodila se greska prilikom otvaranja datoteke!");
+		return -1;
+	}
+
+	fscanf(f, "%s", izraz);
+
+	c=izraz[i];
+	while(c != '\0')
+	{
+
+		dodajNaStog(p, izraz[i]]);
+		i++;
+		c=izraz[i];
+	}
+
+}
+
+
+
+int main() {
+
+	int a = 1;
+	char *naziv = "datoteka.txt";
+
+	list headStog;
+
+	headStog.next = NULL;
+
+	ucitajIzDatoteke(naziv, &headStog);
+
+	ispis(&headStog);
+
+	scanf("%d", &a);
+	return 0;
+}
